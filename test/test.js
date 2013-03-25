c2631f711f59ffbcafe4214c584963311a0f6d33
evanburchard <evan.burchard@gmail.com>
2013-03-25 07:09:49 -0700

    started using wish, moved testing packages to devDependencies in package.json, fixed title

--- test/test.js
+++ test/test.js
@@ -1,15 +1,16 @@
 var assert = require("assert");
 var request = require('supertest');
 var app = require('../app');
+require('wish');
 
 suite('getting pages', function(){
-  test('GET / should return a 200, without errors, and contain the title (JS Arcade)', function(done){
+  test('GET / should return a 200, without errors, and contain the title (Impulse)', function(done){
     request(app).get('/')
     .expect('Content-Type', /text\/html/)
     .expect(200)
     .end(function(err, res){
-      assert.equal(null, err);
-      assert.ok(/Impulse/.test(res.text));
+      wish(null == err);
+      wish(/Impulse/.test(res.text));
       done();
     });
   });
